b17ef664e6b2f4977b13bfff620a6cea2755ee79
Manavshahoff <manavshah.off@gmail.com>
2024-08-19 00:09:07 -0400

    Add MongoDB Atlas in backend

--- App.js
+++ App.js
@@ -289,6 +289,9 @@ app.post("/getActivities", async (req, res) => {
 
 
 
-app.listen(8000, () => {
-  console.log("Server running on port 8000");
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
+

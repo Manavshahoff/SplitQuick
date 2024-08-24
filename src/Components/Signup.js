e4e55b35cb2bd3d111c8e1b0ad62e2df21b6ab92
Manavshahoff <manavshah.off@gmail.com>
2024-08-24 12:48:43 -0400

    add some changes

--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -13,7 +13,7 @@ function Signup() {
     e.preventDefault();
 
     try {
-      await axios.post("https://nodejs-serverless-function-express-603xlm9o8.vercel.app/api/signup", {
+      await axios.post("http://localhost:5000/api/signup", {
         name,
         email,
         password

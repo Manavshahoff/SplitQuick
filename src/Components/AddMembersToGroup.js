ebae5a26f2d8f9a82cfc5753f68349dd9b8610e4
Manavshahoff <manavshah.off@gmail.com>
2024-08-24 13:43:23 -0400

    corrected fetch frinds api in addmembertogroup page

--- src/Components/AddMembersToGroup.js
+++ src/Components/AddMembersToGroup.js
@@ -15,7 +15,7 @@ function AddMembersToGroup() {
   useEffect(() => {
     async function fetchFriends() {
       try {
-        const response = await axios.post("https://nodejs-serverless-function-express-tan-theta.vercel.app/api/addMemberToGroup", {
+        const response = await axios.post("https://nodejs-serverless-function-express-tan-theta.vercel.app/api/getFriends", {
           email: email
         });
         setFriends(response.data.friends);

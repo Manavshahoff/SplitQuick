5606835ac5cb23ac68d44449b5e912f318991f35
Manavshahoff <manavshah.off@gmail.com>
2024-08-23 23:38:55 -0400

    update signup

--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -13,7 +13,7 @@ function Signup() {
     e.preventDefault();
 
     try {
-      await axios.post("https://nodejs-serverless-function-express-e1sne2qsq.vercel.app/signup", {
+      await axios.post("https://nodejs-serverless-function-express-onpnjer3a.vercel.app//signup", {
         name,
         email,
         password

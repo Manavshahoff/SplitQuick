90f88f4acfc7b9dd4157d35390fa30a6d7798f22
Manavshahoff <manavshah.off@gmail.com>
2024-08-24 14:27:24 -0400

    changed the user icon

--- src/Components/Avatar.js
+++ src/Components/Avatar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import profileIcon from '../Icons/User-profile.png';
 import groupIcon from '../Icons/Group-user.png';
-import userIcon from '../Icons/User1.png'; 
+import userIcon from '../Icons/User2.png'; 
 import activityIcon from '../Icons/Activity.png'; 
 
 
